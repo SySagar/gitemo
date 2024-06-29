36bcbe28e3ef70ff44488736bdc0fcebcb6f1df6
Sagar <sysagar07@gmail.com>
2024-06-29 21:34:13 +0530

    feat: server

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,3 +1,17 @@
-module.exports = () => {
-    console.log('hello form server');
-    };
+import express from 'express';
+const app = express();
+const port = 5000; 
+
+app.get('/', (req, res) => {
+  res.send('Server is up and running!');
+});
+
+// Another route that accepts a name parameter and responds with a greeting
+app.get('/greet/:name', (req, res) => {
+  const { name } = req.params;
+  res.send(`Hello, ${name}!`);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
